d7a383c4599dd0f47328248a900213792854ca06
SierraAz <azael.sierram@gmail.com>
2022-09-27 13:49:23 -0600

    Insersion de Forms

---
+++ Codigo/CapaVista/inicio.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CapaVista
+{
+    public partial class inicio : Form
+    {
+        public inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void inicio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void agregarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void verEmpleadosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool abierto = false;
+            foreach (Form f in Application.OpenForms)
+            {
+                if (f.Text == "empleados")
+                {
+                    abierto = true;
+                    f.Focus();
+                    break;
+                }
+            }
+            if (abierto == false)
+            {
+                empleados refe = new empleados();
+                refe.MdiParent = this;
+                refe.Show();
+            }
+        }
+
+        private void diarioToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool abierto = false;
+            foreach (Form f in Application.OpenForms)
+            {
+                if (f.Text == "polizadiario")
+                {
+                    abierto = true;
+                    f.Focus();
+                    break;
+                }
+            }
+            if (abierto == false)
+            {
+                polizadiario refe = new polizadiario();
+                refe.MdiParent = this;
+                refe.Show();
+            }
+        }
+
+        private void ingresoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool abierto = false;
+            foreach (Form f in Application.OpenForms)
+            {
+                if (f.Text == "polizaingreso")
+                {
+                    abierto = true;
+                    f.Focus();
+                    break;
+                }
+            }
+            if (abierto == false)
+            {
+                polizaingreso refe = new polizaingreso();
+                refe.MdiParent = this;
+                refe.Show();
+            }
+        }
+
+        private void egresoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            bool abierto = false;
+            foreach (Form f in Application.OpenForms)
+            {
+                if (f.Text == "polizaegreso")
+                {
+                    abierto = true;
+                    f.Focus();
+                    break;
+                }
+            }
+            if (abierto == false)
+            {
+                polizaegreso refe = new polizaegreso();
+                refe.MdiParent = this;
+                refe.Show();
+            }
+        }
+    }
+}
---
+++ Codigo/CapaVista/verdiario.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace CapaVista
+{
+    partial class verdiario
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(37, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(657, 277);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(303, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Polizas Diario";
+            // 
+            // verdiario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "verdiario";
+            this.Text = "verdiario";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+    }
+}
